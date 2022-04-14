bc9c50a726ed577142e1f8620a25c3eefce43b15
Paul-Andrei Bujor <paul.andrei.bujor@outlook.com>
2022-04-14 16:54:22 +0200

    Ab#12 weapon mechanincs (#5)
    * AB#12 base weapon and added to character
    
    * AB#12 refined bullets and shooting
    
    * AB#12 deleted commented code

---
+++ Assets/Weapons/Scripts/BaseWeaponController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using Assets.Characters;
+using Assets.Weapons.Projectiles;
+using UnityEngine;
+
+namespace Assets.Weapons.Scripts
+{
+    [RequireComponent(typeof(BaseCharacterController))]
+    public class BaseWeaponController : MonoBehaviour
+    {
+        [Header("Projectile")]
+        [SerializeField]
+        private GameObject _projectile;
+
+        [Header("Weapon stats")]
+        [SerializeField]
+        [Tooltip("How many milliseconds between weapon shoots")]
+        private float _rateOfFire = 3f;
+
+        private bool _isShooting;
+        private bool _shootingCoroutineIsRunning;
+
+        private void Start()
+        {
+            _isShooting = false;
+            _shootingCoroutineIsRunning = false;
+        }
+
+        private IEnumerator ShootingAtRateOfFire()
+        {
+            _shootingCoroutineIsRunning = true;
+            while (_isShooting)
+            {
+                OnShoot();
+                yield return new WaitForSeconds(_rateOfFire / 10f);
+            }
+
+            _shootingCoroutineIsRunning = false;
+        }
+
+        private void OnShoot()
+        {
+            var shootingDirection = GetComponent<BaseCharacterController>()?.GetLastDirection() ?? Vector2.right;
+            var projectile = Instantiate(_projectile, transform.position, transform.rotation);
+            var projectileController = projectile.GetComponent<BaseProjectileController>();
+            projectileController.tag = tag;
+            projectileController.Shoot(shootingDirection);
+        }
+
+        protected void OnShootStart()
+        {
+            _isShooting = true;
+            if (!_shootingCoroutineIsRunning)
+            {
+                StartCoroutine(ShootingAtRateOfFire());
+            }
+        }
+
+        protected void OnShootEnd()
+        {
+            _isShooting = false;
+        }
+
+        private void OnDestroy()
+        {
+            StopCoroutine(ShootingAtRateOfFire());
+        }
+    }
+}
